adabf413c9aeb64b9f8cf9e5f7ef63aa3bf675e4
sedrah mourad <sedramourad@icloud.com>
2025-09-26 20:45:53 +0300

    add api project 2

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class book(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     publication_date= models.DateField()
     
     def __str__(self):
-        return f"{self.title} by {self.author} released in {self.publication_date} ."
+        return f"{self.title} by {self.author} released in {self.publication_date} .s"
 
