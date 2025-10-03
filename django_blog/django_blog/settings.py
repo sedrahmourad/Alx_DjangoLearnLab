9b5e8249d25127455e59d92fe7fd72a4d20e4b51
sedrah mourad <sedramourad@icloud.com>
2025-10-03 15:54:18 +0300

    add first part 2

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,7 +77,11 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME' : 'django_blog',
+        'USER' : 'ROOT',
+        'PASSWORD' : 'sedrah41',
+        'HOST' : 'localhost',
+        'PORT' : '5432'
     }
 }
 
